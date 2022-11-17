36c870cff53ed035618486c29b97c1d15daf0279
foxthefox <16841643+foxthefox@users.noreply.github.com>
2022-11-17 23:06:39 +0100

    new0.9.1

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import Fritz from './lib/fritz_ahaapi.js';
-import FritzEmu from './lib/fritz_mockserver_new.js';
-//const Fritz = require('./lib/fritz_ahaapi.js');
-//const FritzEmu = require('./lib/fritz_mockserver_new.js');
+import FritzEmu from './lib/fritz_mockserver.js';
 
 export { Fritz, FritzEmu };
